23da8f6eb7abec9c4f3faae768525c23ee63ee4f
sh-navid <shamshirsaz.navid@live.com>
2024-02-22 20:06:16 +0330

    Setup Test5

--- terminal.py
+++ terminal.py
@@ -9,7 +9,7 @@ from src.format import KotlinFormatter
 
 root = sys.path[0]
 
-files = ["Test1", "Test2", "Test3","Test4"]
+files = ["Test1", "Test2", "Test3", "Test4", "Test5"]
 
 for f in files:
     test_in = root + f"/testcase/{f}.kt"
