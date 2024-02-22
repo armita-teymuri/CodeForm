65fa1383d8ad689b01ebb0fd23e5c7391ed7ef00
sh-navid <shamshirsaz.navid@live.com>
2024-02-22 20:06:08 +0330

    Setup Test5

---
+++ testcase/Test5.formed.kt
@@ -0,0 +1,12 @@
+fun main(){
+	// --open-formatter-skip-next-line
+	println("Testcase number 5" )
+	// Format blocks without {}
+	for(i in 1..5)
+	for(j in 1..5)
+	for(k in 1..5)
+	println("$i $j $k")
+	var x=4
+	while(x-->0)
+	println(x)
+	}
