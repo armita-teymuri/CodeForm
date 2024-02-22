bea13c5e3d1842184437350e74972cfcfcca7e50
sh-navid <shamshirsaz.navid@live.com>
2024-02-22 19:20:20 +0330

    Update test execution instruction

--- extension/vscode/test/extension.test.js
+++ extension/vscode/test/extension.test.js
@@ -3,6 +3,7 @@ const vscode = require('vscode');
 const extension = require('../extension');
 
 // Install https://marketplace.visualstudio.com/items?itemName=ms-vscode.extension-test-runner
+// From left side of IDE; Under Extensions; Click on `testing` button
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
