5f8118cab8f029ae4767dddc5e1c3026493cd96a
sh-navid <shamshirsaz.navid@live.com>
2024-02-23 11:52:14 +0330

    Include a method to indent blocks that do not have {} braces

--- src/format.py
+++ src/format.py
@@ -1,26 +1,52 @@
 import re
 
+TAB = "    "
+
 
 class Formatter:  # Default is kotlin
     @staticmethod
-    def fix_indentation(content):
+    def fix_indentation_for_blocks_with_brace(content: str):
         # Space to keep clean code
         clean = ""
+        tab_num = 0
 
-        num = 0
-        tab = "\t"
-
-        for c in content:
+        for i, c in enumerate(content):
+            # print(i)
             if c == "{":
-                num += 1
-            elif c == "}" and num > 0:
-                num -= 1
+                tab_num += 1
+            elif c == "}" and tab_num > 0:
+                tab_num -= 1
 
             clean += c
 
             if c == "\n":
-                for _ in range(0, num):
-                    clean += tab
+                for _ in range(0, tab_num):
+                    clean += TAB
+
+        return clean
+
+    @staticmethod
+    def fix_indentation_for_blocks_without_brace(content: str):
+        # Space to keep clean code
+        clean = ""
+        tab_num = 0
+
+        lines = content.splitlines()
+
+        for line in lines:
+            trimed_line = line.strip()
+
+            for _ in range(0, tab_num):
+                clean += TAB
+
+            clean += line
+
+            if trimed_line.startswith("for(") or trimed_line.startswith("while("):
+                tab_num += 1
+            elif tab_num > 0:
+                tab_num = 0
+
+            clean += "\n"
 
         return clean
 
